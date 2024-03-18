f21580ba79f282238efa55146f33d4ea904643cb
Vlad Romanov <vladromeur@gmail.com>
2024-03-18 16:18:16 +0500

    Примеры из курса Владлена

--- VladlenMininLessons/TSIn70mins/types.js
+++ VladlenMininLessons/TSIn70mins/types.js
@@ -1,2 +1,15 @@
-var st = 'Hello';
-console.log(st);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var isFerching = true;
+var isLoading = false;
+var int = 30;
+var float = 4.2;
+var message = 'Hello Typescript';
+var numberArray = [1, 1, 2, 3, 4];
+var numberArray2 = [1, 1, 2, 3, 5];
+var words = ['Hello', 'Typescript'];
+console.log(typeof words);
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Хайзенберг');
--- VladlenMininLessons/TSIn70mins/types.ts
+++ VladlenMininLessons/TSIn70mins/types.ts
@@ -1,3 +1,21 @@
-const st: string ='Hello'
+export{}
+const isFerching: boolean = true
+const isLoading: boolean = false
 
-console.log(st)
+const int: number = 30
+const float: number = 4.2
+
+const message: string = 'Hello Typescript'
+
+const numberArray: number[] = [1, 1, 2, 3, 4] 
+const numberArray2: Array<number> = [1, 1, 2, 3, 5]
+
+const words: string[] = ['Hello', 'Typescript']
+
+console.log(typeof words)
+
+function sayMyName(name: string): void {
+    console.log(name)
+}
+
+sayMyName('Хайзенберг')

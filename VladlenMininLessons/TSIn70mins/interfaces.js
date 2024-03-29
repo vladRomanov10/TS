b9fc255fc3a2e69c7474d44300c64a96d17e5143
Vlad Romanov <vladromeur@gmail.com>
2024-03-29 15:39:53 +0500

    Примеры из курса Владлена

--- VladlenMininLessons/TSIn70mins/interfaces.js
+++ VladlenMininLessons/TSIn70mins/interfaces.js
@@ -5,3 +5,25 @@ var employee = {
     name: 'Vlad',
     street: 'Otegen batyr'
 };
+var employee2 = {
+    id: 2,
+    name: 'Nike',
+    street: 'Nekrasova',
+    getNameAndStreet: function () {
+        console.log(this.name + ' ' + this.street);
+    },
+};
+console.log(employee2.getNameAndStreet());
+var User = /** @class */ (function () {
+    function User(userId, userName, userAge) {
+        this.id = userId;
+        this.name = userName;
+        this.age = userAge;
+    }
+    User.prototype.getFullName = function (surename) {
+        return this.name + ' ' + surename;
+    };
+    return User;
+}());
+var user = new User(3, 'Yarik', 29);
+console.log(user.getFullName('Tiss'));
--- VladlenMininLessons/TSIn70mins/interfaces.ts
+++ VladlenMininLessons/TSIn70mins/interfaces.ts
@@ -12,3 +12,45 @@
     street: 'Otegen batyr'
  }
 
+//Наследование интерфейсов
+
+interface IUserNameAndStreet extends IUser {
+   getNameAndStreet(): void
+}
+
+const employee2: IUserNameAndStreet = {
+   id: 2,
+   name: 'Nike',
+   street: 'Nekrasova',
+   getNameAndStreet(): void {
+      console.log(this.name + ' ' + this.street)
+   },
+}
+
+console.log(employee2.getNameAndStreet())
+
+//Интерфейсы и классы 
+
+interface IUserLogin {
+   id: number
+   name: string
+   getFullName(surename: string): string
+}
+
+class User implements IUserLogin {
+   id: number
+   name: string
+   age: number
+   constructor(userId: number, userName: string, userAge: number) {
+      this.id = userId
+      this.name = userName
+      this.age = userAge
+   }
+   getFullName(surename: string): string {
+      return this.name + ' ' + surename
+   }
+}
+
+const user = new User (3, 'Yarik', 29)
+
+console.log(user.getFullName('Tiss'))
